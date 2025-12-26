aa5bd19c4d38881a3c2d98ec71947a0f760e1816
Rasmustofth <rasmustofthermansen@gmail.com>
2025-12-26 17:13:32 +0100

    Update url

--- app/modules/ScrapeDanskeSpil.ts
+++ app/modules/ScrapeDanskeSpil.ts
@@ -1,6 +1,6 @@
 export async function GetData()
 {
-    const url = "http://127.0.0.1:8000/danskespil"
+    const url = "https://srv589522.hstgr.cloud/fastapi-api/danskespil"
     const response = await fetch(url);
     if (!response.ok) {
       console.error("Request failed with status:", response.status);
